3655b7b84a6b54c8c666280717d85c5c70333228
Belkis Vaq <55562967+BelkisVasquez0609@users.noreply.github.com>
2022-11-24 21:14:28 -0400

    Override Methods of visitor

--- RecetaParser/AnalizadorReceta.cs
+++ RecetaParser/AnalizadorReceta.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Antlr4.Runtime.Misc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,32 @@ using System.Threading.Tasks;
 
 namespace RecetaParser
 {
-    internal class AnalizadorReceta : ProyectoRecetarioBaseVisitor<string> 
+    internal class AnalizadorReceta : ProyectoRecetarioBaseVisitor<string>
     {
+        public override string VisitInformacion([NotNull] ProyectoRecetarioParser.InformacionContext context)
+        {
+            return base.VisitInformacion(context);
+        }
+
+        public override string VisitElaboracion([NotNull] ProyectoRecetarioParser.ElaboracionContext context)
+        {
+            return base.VisitElaboracion(context);
+        }
+
+        public override string VisitIngredientes([NotNull] ProyectoRecetarioParser.IngredientesContext context)
+        {
+            return base.VisitIngredientes(context);
+        }
+
+        public override string VisitProgram([NotNull] ProyectoRecetarioParser.ProgramContext context)
+        {
+            return base.VisitProgram(context);
+        }
+
+        public override string VisitReceta([NotNull] ProyectoRecetarioParser.RecetaContext context)
+        {
+            
+            return base.VisitReceta(context);
+        }
     }
 }
--- RecetaParser/Program.cs
+++ RecetaParser/Program.cs
@@ -1,10 +1,12 @@
 ﻿using Antlr4.Runtime;
+using RecetaParser;
 
 var input = CharStreams.fromPath(@"..\..\..\Recetario.txt");
 
 var lexer = new ProyectoRecetarioLexer(input); 
 var tokenStream = new CommonTokenStream(lexer); 
 var parser = new ProyectoRecetarioParser(tokenStream); 
-var tree = parser.program(); 
+var tree = parser.program();
 
-//var visitor
+var visitor = new AnalizadorReceta();
+visitor.Visit(tree);
